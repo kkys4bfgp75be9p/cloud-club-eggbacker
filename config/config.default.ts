0a271c994241ebb956936d19362e613a3ebe1673
Alalabu <253993420@qq.com>
2019-03-22 15:32:51 +0800

    变更云社团运行端口

--- config/config.default.ts
+++ config/config.default.ts
@@ -68,7 +68,7 @@ export default (appInfo: EggAppInfo) => {
    */
   config.cluster = {
     listen: {
-      port: 7003,
+      port: 18000,
       hostname: '127.0.0.1',
       // path: '/var/run/egg.sock',
     },
--- test/app/service/Test.test.js
+++ test/app/service/Test.test.js
@@ -7,21 +7,9 @@ describe('test/app/service/Test.test.js', () => {
     before(async () => {
         ctx = bootstrap_1.app.mockContext();
     });
-    // it('formartDateTime ::: ', async () => {
-    //   const result = ctx.helper.formartDateTime(new Date(), true);
-    //   console.log('result:: ', result);
-    // });
-    //   it('sayHi', async () => {
-    //     const result = await ctx.service.gift.getActiveInfo();
-    //     console.log('getActiveInfo ::',  result);
-    //     // console.log('getActiveInfo ::',  util.inspect( result, true, 3));
-    //     // assert(result === 'hi, egg');
-    //   });
     it('sayHi', async () => {
         const result = await ctx.service.area.getCityList({ provincecode: '610000' });
         console.log('getActiveInfo ::', result);
-        // console.log('getActiveInfo ::',  util.inspect( result, true, 3));
-        // assert(result === 'hi, egg');
     });
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiVGVzdC50ZXN0LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiVGVzdC50ZXN0LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBRUEsa0RBQXlDO0FBQ3pDLGlDQUFpQztBQUVqQyxRQUFRLENBQUMsK0JBQStCLEVBQUUsR0FBRyxFQUFFO0lBQzdDLElBQUksR0FBWSxDQUFDO0lBRWpCLE1BQU0sQ0FBQyxLQUFLLElBQUksRUFBRTtRQUNoQixHQUFHLEdBQUcsZUFBRyxDQUFDLFdBQVcsRUFBRSxDQUFDO0lBQzFCLENBQUMsQ0FBQyxDQUFDO0lBRUgsMkNBQTJDO0lBQzNDLGlFQUFpRTtJQUNqRSxzQ0FBc0M7SUFDdEMsTUFBTTtJQUVSLDhCQUE4QjtJQUM5Qiw2REFBNkQ7SUFDN0QsZ0RBQWdEO0lBQ2hELDJFQUEyRTtJQUMzRSx1Q0FBdUM7SUFDdkMsUUFBUTtJQUVOLEVBQUUsQ0FBQyxPQUFPLEVBQUUsS0FBSyxJQUFJLEVBQUU7UUFDckIsTUFBTSxNQUFNLEdBQUcsTUFBTSxHQUFHLENBQUMsT0FBTyxDQUFDLElBQUksQ0FBQyxXQUFXLENBQUMsRUFBQyxZQUFZLEVBQUUsUUFBUSxFQUFDLENBQUMsQ0FBQztRQUM1RSxPQUFPLENBQUMsR0FBRyxDQUFDLGtCQUFrQixFQUFHLE1BQU0sQ0FBQyxDQUFDO1FBQ3pDLG9FQUFvRTtRQUNwRSxnQ0FBZ0M7SUFDbEMsQ0FBQyxDQUFDLENBQUM7QUFDTCxDQUFDLENBQUMsQ0FBQyJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiVGVzdC50ZXN0LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiVGVzdC50ZXN0LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBRUEsa0RBQXlDO0FBQ3pDLGlDQUFpQztBQUVqQyxRQUFRLENBQUMsK0JBQStCLEVBQUUsR0FBRyxFQUFFO0lBQzdDLElBQUksR0FBWSxDQUFDO0lBRWpCLE1BQU0sQ0FBQyxLQUFLLElBQUksRUFBRTtRQUNoQixHQUFHLEdBQUcsZUFBRyxDQUFDLFdBQVcsRUFBRSxDQUFDO0lBQzFCLENBQUMsQ0FBQyxDQUFDO0lBRUgsRUFBRSxDQUFDLE9BQU8sRUFBRSxLQUFLLElBQUksRUFBRTtRQUNyQixNQUFNLE1BQU0sR0FBRyxNQUFNLEdBQUcsQ0FBQyxPQUFPLENBQUMsSUFBSSxDQUFDLFdBQVcsQ0FBQyxFQUFDLFlBQVksRUFBRSxRQUFRLEVBQUMsQ0FBQyxDQUFDO1FBQzVFLE9BQU8sQ0FBQyxHQUFHLENBQUMsa0JBQWtCLEVBQUcsTUFBTSxDQUFDLENBQUM7SUFFM0MsQ0FBQyxDQUFDLENBQUM7QUFDTCxDQUFDLENBQUMsQ0FBQyJ9
